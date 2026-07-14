9bd0a443f943f100913216eb41f162a0b1317f0d
Nasrullah <halo.nasrullah@gmail.com>
2026-07-14 16:38:17 +0800

    feat: routing dan controller dasar

--- tokoku/routes/web.php
+++ tokoku/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaContoller;
+use App\Http\Controllers\ProdukContoller;
+use App\Http\Controllers\KategoriContoller;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BerandaContoller::class,'index'])->name('beranda');
+Route::get('/tentang', [BerandaContoller::class,'tentang'])->name('tentang');
+Route::get('/produk', [ProdukContoller::class,'index'])->name('produk.index');
+Route::get('/produk/{id}', [ProdukContoller::class,'show'])->name('produk.show');
+Route::get('/kategori/{slug}', [KategoriContoller::class,'show'])->name('kategori.show');
