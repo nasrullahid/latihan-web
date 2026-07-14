b7fe1c5bf4daceb8b26273d8f96e4cf2139b9710
Nasrullah <halo.nasrullah@gmail.com>
2026-07-14 16:40:26 +0800

    fix: typo controller name

--- tokoku/routes/web.php
+++ tokoku/routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BerandaContoller;
-use App\Http\Controllers\ProdukContoller;
-use App\Http\Controllers\KategoriContoller;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\KategoriController;
 
-Route::get('/', [BerandaContoller::class,'index'])->name('beranda');
-Route::get('/tentang', [BerandaContoller::class,'tentang'])->name('tentang');
-Route::get('/produk', [ProdukContoller::class,'index'])->name('produk.index');
-Route::get('/produk/{id}', [ProdukContoller::class,'show'])->name('produk.show');
-Route::get('/kategori/{slug}', [KategoriContoller::class,'show'])->name('kategori.show');
+Route::get('/', [BerandaController::class,'index'])->name('beranda');
+Route::get('/tentang', [BerandaController::class,'tentang'])->name('tentang');
+Route::get('/produk', [ProdukController::class,'index'])->name('produk.index');
+Route::get('/produk/{id}', [ProdukController::class,'show'])->name('produk.show');
+Route::get('/kategori/{slug}', [KategoriController::class,'show'])->name('kategori.show');
